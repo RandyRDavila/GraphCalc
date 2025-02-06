d8385da0a5ff33b768b9d76784db715882f6f800
Randy Davila <randyrdavila@gmail.com>
2025-02-06 09:43:45 -0600

    added polytope functions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="graphcalc",
-    version="0.1.13",
+    version="0.1.14",
     author="Randy Davila",
     author_email="rrd6@rice.edu",
     description="A Python package for graph computation functions",
