46efca147b219e37c57825c0965d3936235e63e9
Randy Davila <randy.davila@relational.ai>
2025-08-05 09:39:49 -0500

    Bump version to 1.2.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'graphcalc'
 copyright = '2024, Randy Davila, PhD'
 author = 'Randy Davila, PhD'
-release = '0.2.1'
+release = '1.2.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- src/graphcalc/__init__.py
+++ src/graphcalc/__init__.py
@@ -30,7 +30,7 @@ GraphCalc relies on:
 
 """
 
-__version__ = "0.2.1"
+__version__ = "1.2.2"
 
 from graphcalc.core import *
 from graphcalc.data import *
