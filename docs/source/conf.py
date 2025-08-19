3c82567e288c73eaf2f8de52838ce5dada540be1
Randy Davila <randy.davila@relational.ai>
2025-08-19 09:40:48 -0500

    updated to v1.2.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'graphcalc'
 copyright = '2024, Randy Davila, PhD'
 author = 'Randy Davila, PhD'
-release = '1.2.3'
+release = '1.2.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
